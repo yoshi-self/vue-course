e1e0ea6d2766290459f10d14b19f502872ea77b0
Yoshi <crispy245@gmail.com>
2020-03-29 22:06:39 +0900

    Add components

---
+++ components/src/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <app-server-status v-for="server in 5"></app-server-status>
+    </div>
+</template>
+
+<script>
+import ServerStatus from './ServerStatus.vue';
+
+export default {
+    data() {
+        return {
+            status: 'Critical',
+        };
+    },
+    methods: {
+        changeStatus() {
+            this.status = 'Normal';
+        }
+    },
+    components: {
+        'app-server-status': ServerStatus,
+    },
+}
+</script>
