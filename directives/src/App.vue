cd036725fe74dcf7f1766761e9e169b5f33d1608
Yoshi <crispy245@gmail.com>
2020-03-29 22:07:36 +0900

    Add directives

---
+++ directives/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives</h1>
+                <p v-text="'hoge'"></p>
+                <p v-html="'<strong>hhhh</strong>'"></p>
+
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives</h1>
+                <p v-highlight:background.delayed="'red'">Color This</p>
+                <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay:500}">Color This</p>
+                <p v-highlight="'red'">Color This</p>
+                <p v-html="'<strong>hhhh</strong>'"></p>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    directives: {
+        'local-highlight': {
+            bind(el, binding, vnode) {
+                console.log(binding)
+                //el.style.backgroundColor = binding.value;
+                var delay = 0;
+                if(binding.modifiers['delayed']) {
+                    delay = 2000;
+                }
+                if(binding.modifiers['blink']) {
+                    const {mainColor, secondColor} = binding.value;
+//                    let mainColor = binding.value.mainColor;
+//                    let secondColor= binding.value.secondColor;
+                    let currentColor = mainColor;
+
+                    setTimeout(() => {
+                        setInterval(() => {
+                            currentColor = currentColor == mainColor ? secondColor : mainColor;
+                            if(binding.arg == 'background') {
+                                el.style.backgroundColor = currentColor;
+                            }
+                            else {
+                                el.style.color = currentColor;
+                            }
+                        },binding.value.delay);
+                    }, delay);
+
+                }
+                else {
+                    setTimeout(() => {
+                        if(binding.arg == 'background') {
+                            el.style.backgroundColor = binding.value.mainColor;
+                        }
+                        else {
+                            el.style.color = binding.value.mainColor;
+                        }
+                    }, delay);
+                }
+            },
+        },
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ directives/src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.directive('highlight', {
+    bind(el, binding, vnode) {
+        //el.style.backgroundColor = 'green';
+        console.log(binding)
+        //el.style.backgroundColor = binding.value;
+        var delay = 0;
+        if(binding.modifiers['delayed']) {
+            delay = 2000;
+        }
+        setTimeout(() => {
+            if(binding.arg == 'background') {
+                el.style.backgroundColor = binding.value;
+            }
+            else {
+                el.style.color = binding.value;
+            }
+        }, delay);
+
+    },
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
