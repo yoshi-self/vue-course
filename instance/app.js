3cee6269f217ab36654ff829e09f3f2375c60f6d
Yoshi <crispy245@gmail.com>
2020-03-29 21:58:16 +0900

    Add instance

---
+++ instance/app.js
@@ -0,0 +1,68 @@
+Vue.component('hello', {
+    template: '<h1>hello</h1>',
+});
+
+var vm1 = new Vue({
+  data: {
+    title: 'The VueJS Instance',
+    showParagraph: false
+  },
+  methods: {
+    show: function() {
+      this.showParagraph = true;
+      this.updateTitle('The VueJS Instance (Updated)');
+      console.log(this.$refs);
+    },
+    updateTitle: function(title) {
+      this.title = title;
+    }
+  },
+  computed: {
+    lowercaseTitle: function() {
+      return this.title.toLowerCase();
+    }
+  },
+  watch: {
+    title: function(value) {
+      alert('Title changed, new value: ' + value);
+    }
+  }
+});
+
+vm1.$mount('#app1');
+
+var vm2 = new Vue({
+  el: '#app2',
+  data: {
+    title: 'The second Instance',
+    showParagraph: false
+  },
+  methods: {
+    onChange() {
+        vm1.title = 'changed';
+    },
+    show: function() {
+      this.showParagraph = true;
+      this.updateTitle('The VueJS Instance (Updated)');
+    },
+    updateTitle: function(title) {
+      this.title = title;
+    }
+  },
+  computed: {
+    lowercaseTitle: function() {
+      return this.title.toLowerCase();
+    }
+  },
+  watch: {
+    title: function(value) {
+      alert('Title changed, new value: ' + value);
+    }
+  }
+});
+
+//var vm3 = new Vue({
+//    el: 'hello',
+//    template: '<h1>hello</h1>',
+//});
+
