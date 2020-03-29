fb3d1c372882422616948cc20feb682ae119b696
Yoshi <crispy245@gmail.com>
2020-03-29 22:09:13 +0900

    Add advanced_component

---
+++ advanced_component/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>Default Content</p>
+                    </component>
+                </keep-alive>
+                <!--
+                <app-quote>
+                    <h2 slot="title">{{ quoteTitle }}</h2>
+                    <p>Slotted quote</p>
+                </app-quote>
+                -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Quote from './components/Quote.vue';
+import Author from './components/Author.vue';
+import New from './components/New.vue';
+export default {
+    data() {
+        return {
+            quoteTitle: 'The Quote Title',
+            selectedComponent: 'appQuote',
+        };
+    },
+    components: {
+        appQuote: Quote,
+        appAuthor: Author,
+        appNew: New,
+    },
+    
+}
+</script>
+
+<style scoped>
+h2 {
+    color: red;
+}
+</style>
---
+++ advanced_component/src/components/New.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h3>New Quote</h3>
+        <button @click="counter++">Increase</button>
+        <p>{{ counter }}</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            counter: 0,
+        };
+    },
+    activated() {
+        console.log('activated');
+    },
+    deactivated() {
+        console.log('deactivated');
+    },
+}
+</script>
+
+<style scoped>
+div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding: 30px;
+    margin: 30px auto;
+    text-align: center;
+}
+
+h2 {
+    color: blue;
+}
+
+.title {
+    font-style: italic;
+}
+</style>
