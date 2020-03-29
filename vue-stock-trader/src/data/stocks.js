cca8f147213304e3e8b14036159f312d4983e9ad
Yoshi <crispy245@gmail.com>
2020-03-29 22:15:49 +0900

    Add vue-stock-trader

---
+++ vue-stock-trader/src/data/stocks.js
@@ -0,0 +1,7 @@
+export default [
+  { id: 1, name: 'BMW', price: 110 },
+  { id: 2, name: 'Google', price: 200 },
+  { id: 3, name: 'BMW', price: 250 },
+  { id: 4, name: 'Apple', price: 110 },
+  { id: 5, name: 'Twitter', price: 8 },
+]
